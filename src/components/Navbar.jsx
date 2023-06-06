498614c93d04291a0a4c4201051895338514374e
gogol54 <uniuffs@gmail.com>
2023-06-06 19:04:08 -0300

    conclude diff status

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,16 +28,16 @@ const Navbar = () => {
     <Box>
        <Container>
         <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{borderRadius: '10px'}}>
-        
+          <a className="navbar-brand" href="/">Calhas Nobres</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">
-                
-                    Página Inicial 
-                <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/">Página Inicial <span className="sr-only">(current)</span></a>
               </li>
-           
               <li className="nav-item dropdown active">
                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Serviços
